711c2e6d4c8c5c3711beae253846668e3f571765
Dragoslav Tamindzija <dragodev@protonmail.com>
2022-09-21 21:52:53 +0200

    basic types and parsing file

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use std::{env, fs, process::exit};
+use colored::Colorize;
+
+mod types;
+use types::*;
+
+
+fn main() {
+    // load file name
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("{}: missing file name",  "error".red());
+        exit(1);
+    }
+    let test_config: &String = &args[1];
+
+    // check if the file/path exists
+    let file_contents = fs::read_to_string(test_config);
+    if file_contents.is_err(){
+        println!("{}: opening file: {}",  "error".red(), file_contents.err().unwrap());
+        exit(1);
+    }
+    let file_contents = file_contents.unwrap();
+
+    // parsing config file into json
+    let master_struct: Result<MasterStruct, serde_json::Error> = serde_json::from_str(&file_contents);
+    if master_struct.is_err(){
+        println!("{}: parsing config file: {}",  "error".red(), master_struct.err().unwrap());
+        exit(1);
+    }
+    let master_struct = master_struct.unwrap();
+    println!("{:?}", master_struct);
+}
---
+++ src/types.rs
@@ -0,0 +1,59 @@
+use serde_derive::{Deserialize};
+
+#[derive(Deserialize, Debug)]
+pub struct MasterStruct{
+    pub config: Config,
+    pub tests: Vec<Test>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Config{
+    /// url to which requests are sent
+    pub base_url: String,
+    /// port to which request are sent
+    /// -1 if no port is required
+    pub port: i32,
+    /// timeout between each test in ms
+    /// 0 if no timeout
+    pub timeout: u128,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Test{
+    // request
+    pub end_point: String,
+    pub method: HttpMethod,
+    pub headers: Vec<Header>,
+    pub params: Vec<UrlParams>,
+    pub payload: Option<String>,
+
+    // response
+    pub response_code: u16,
+    pub response_body: Option<String>,
+    pub response_headers: Option<Vec<String>>,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Header{
+    pub header: String,
+    pub value: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct UrlParams{
+    pub key: String,
+    pub value: String,
+}
+
+#[derive(Deserialize, Debug)]
+pub enum HttpMethod{
+    GET,
+    HEAD,
+    POST,
+    PUT,
+    DELETE,
+    CONNECT,
+    OPTIONS,
+    TRACE,
+    PATCH,
+}
