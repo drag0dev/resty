5579c91b36a0e18cf61e80dbcca83dc3d29d38b2
Dragoslav Tamindzija <dragodev@protonmail.com>
2022-10-16 18:07:51 +0200

    show where the diff starts in the header value

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,5 +1,5 @@
 use crate::types::Header;
-use reqwest::header::HeaderMap;
+use reqwest::header::{HeaderMap,HeaderValue};
 
 pub fn header_match(header: &Header, result_headers: &HeaderMap) -> bool{
     if let Some(res_header) = result_headers.get(&header.header){
@@ -10,4 +10,30 @@ pub fn header_match(header: &Header, result_headers: &HeaderMap) -> bool{
         return res_header_value.unwrap() == header.value;
     }
     false
+}
+
+pub fn slice<'a>(test_header: &'a Header, res_header: &HeaderValue) -> (&'a str, usize){
+    let mut start = 0;
+    let mut end;
+
+    // find start of the diff
+    for (i, (t, r)) in test_header.value.chars()
+        .zip(res_header.to_str().unwrap().chars())
+        .enumerate(){
+            if t != r{
+                start = i;
+                break;
+            }
+    }
+
+    end = start + 15;
+    if end >= test_header.value.len(){
+        end = test_header.value.len()-1;
+    }
+    let diff_char = start;
+    if start >= 15  {
+        start -= 15;
+    }
+
+    (&test_header.value[start..end], diff_char)
 }
