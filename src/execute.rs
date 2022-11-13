fa1428e8a2b438f535f341f7af5d037f10968521
Dragoslav Tamindzija <dragodev@protonmail.com>
2022-11-13 20:23:46 +0100

    functions executing tests in a separate mod

---
+++ src/execute.rs
@@ -0,0 +1,101 @@
+use colored::Colorize;
+use std::time::Duration;
+use crate::{
+    types::MasterStruct,
+    client::Client,
+    helpers::*,
+    wsconfig,
+};
+
+
+pub async fn http(master_struct: MasterStruct) -> (u32, u32){
+    let master_client = Client::new(&master_struct.config);
+    if master_client.is_err(){
+        println!("{} setting up http client: {}", "error".red().bold(), master_client.err().unwrap());
+        std::process::exit(1);
+    }
+
+    let mut success = 0;
+    let mut failed = 0;
+    let timeout = if master_struct.config.timeout.is_some(){
+        master_struct.config.timeout.unwrap()
+    }else{
+        0
+    };
+
+    let master_client = master_client.unwrap();
+    for (i, t) in master_struct.tests.iter().enumerate(){
+        let result = master_client.exec_test(t).await;
+        if result.is_err(){
+            println!("{}: executing a test {}", "error".red().bold(), i+1);
+            failed += 1;
+        }else{
+            let result = result.unwrap();
+            let mut failed_check: bool = false;
+
+            // check status code
+            if t.response_code != result.status().as_u16(){
+                println!("{} ({}) - response code not matching {} != {}",
+                "fail".red().bold(), i+1, t.response_code, result.status().as_u16());
+                failed_check = true;
+            }
+
+            // check headers if required
+            if let Some(test_headers) = &t.response_headers{
+                let mut first: bool = false;
+                let res_headers = result.headers();
+                for (header_i, h) in test_headers.iter().enumerate(){
+                    if !header_match(h,  res_headers){
+                        if !first{
+                            println!("{} ({}) - headers not matching:", "fail".red().bold(), i+1);
+                            failed_check = true;
+                            first = true;
+                        }
+                        println!("\t({}) {} not matching ", header_i+1, h.header);
+                        if let Some(value) = res_headers.get(&h.header){
+                            println!("\t\tTest header value: {}", h.value);
+                            println!("\t\tResult header value: {}", value.to_str().unwrap());
+                        }else{
+                            println!("\t\t  missing header");
+                        }
+                    }
+                }
+            }
+
+            // check body if required
+            if let Some(body) = &t.response_body{
+                let res_body = result.bytes().await;
+                if res_body.is_err(){
+                    println!("{} ({}) - error getting response body", "fail".red().bold(), i+1);
+                    failed_check = true;
+                }else{
+                    let res_body = res_body.unwrap();
+                    let res_body_str = res_body.iter().map(|b| *b as char).collect::<String>();
+                    if body_match(body, &res_body_str, i){
+                        failed_check = true;
+                    }
+                }
+            }
+
+            if failed_check{
+                failed += 1;
+            }else{
+                success += 1;
+                println!("{} ({}) - /{}", "success".green().bold(), i+1, t.request_end_point);
+            }
+        }
+
+        if timeout > 0{
+            std::thread::sleep(Duration::from_millis(timeout as u64));
+        }
+    }
+    (success, failed)
+}
+
+pub async fn ws(master_struct: wsconfig::MasterStruct) -> (u32, u32){
+    let mut success = 0;
+    let mut failed = 0;
+
+
+    (success, failed)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,12 @@
 use std::{env, fs, process::exit};
 use colored::Colorize;
-use std::time::Duration;
 
 mod types;
 mod client;
 mod helpers;
 mod clientws;
 mod wsconfig;
-use types::*;
-use client::Client;
-use helpers::*;
-
-enum TestType{
-    HTTP,
-    WS,
-}
+mod execute;
 
 #[tokio::main]
 async fn main() {
@@ -25,18 +17,10 @@ async fn main() {
         exit(1);
     }
 
-    // determine if its http or ws tests
-    let test_type = if args[1].starts_with("ws"){
-        TestType::WS
-    }else if args[1].starts_with("http"){ // covers both http and https
-        TestType::HTTP
-    }else {
-        panic!("Missing or wrong uri in the test file name \"{}\"!", args[1]);
-    };
-
     for test_config in args.iter().skip(1){
         println!("Running {}", test_config);
         println!("----------------------------------------");
+
         // check if the file/path exists
         let file_contents = fs::read_to_string(test_config);
         if file_contents.is_err(){
@@ -45,93 +29,32 @@ async fn main() {
         }
         let file_contents = file_contents.unwrap();
 
-        // parsing config file into json
-        let master_struct: Result<MasterStruct, serde_json::Error> = serde_json::from_str(&file_contents);
-        if master_struct.is_err(){
-            println!("{}: parsing config file: {}",  "error".red().bold(), master_struct.err().unwrap());
-            exit(1);
-        }
-        let master_struct = master_struct.unwrap();
-        let master_client = Client::new(&master_struct.config);
-        if master_client.is_err(){
-            println!("{} setting up client: {}", "error".red().bold(), master_client.err().unwrap());
-            std::process::exit(1);
-        }
+        let success;
+        let failed;
 
-        let mut success = 0;
-        let mut failed = 0;
-        let timeout = if master_struct.config.timeout.is_some(){
-            master_struct.config.timeout.unwrap()
-        }else{
-            0
+        // determine type of tests
+        if args[1].starts_with("ws"){
+            let master_struct: Result<wsconfig::MasterStruct, serde_json::Error> = serde_json::from_str(&file_contents);
+            if master_struct.is_err(){
+                println!("{}: parsing ws test file: {}",  "error".red().bold(), master_struct.err().unwrap());
+                exit(1);
+            }
+            let master_struct = master_struct.unwrap();
+            (success, failed) = execute::ws(master_struct).await;
+
+        }else if args[1].starts_with("http"){ // covers both http and https
+            let master_struct: Result<types::MasterStruct, serde_json::Error> = serde_json::from_str(&file_contents);
+            if master_struct.is_err(){
+                println!("{}: parsing http test file: {}",  "error".red().bold(), master_struct.err().unwrap());
+                exit(1);
+            }
+            let master_struct = master_struct.unwrap();
+            (success, failed) = execute::http(master_struct).await;
+
+        }else {
+            panic!("{}: missing or wrong schema in the test file name \"{}\"!",
+                "error".red().bold(), args[1]);
         };
-
-        let master_client = master_client.unwrap();
-        for (i, t) in master_struct.tests.iter().enumerate(){
-            let result = master_client.exec_test(t).await;
-            if result.is_err(){
-                println!("{}: executing a test {}", "error".red().bold(), i+1);
-                failed += 1;
-            }else{
-                let result = result.unwrap();
-                let mut failed_check: bool = false;
-
-                // check status code
-                if t.response_code != result.status().as_u16(){
-                    println!("{} ({}) - response code not matching {} != {}",
-                    "fail".red().bold(), i+1, t.response_code, result.status().as_u16());
-                    failed_check = true;
-                }
-
-                // check headers if required
-                if let Some(test_headers) = &t.response_headers{
-                    let mut first: bool = false;
-                    let res_headers = result.headers();
-                    for (header_i, h) in test_headers.iter().enumerate(){
-                        if !header_match(h,  res_headers){
-                            if !first{
-                                println!("{} ({}) - headers not matching:", "fail".red().bold(), i+1);
-                                failed_check = true;
-                                first = true;
-                            }
-                            println!("\t({}) {} not matching ", header_i+1, h.header);
-                            if let Some(value) = res_headers.get(&h.header){
-                                println!("\t\tTest header value: {}", h.value);
-                                println!("\t\tResult header value: {}", value.to_str().unwrap());
-                            }else{
-                                println!("\t\t  missing header");
-                            }
-                        }
-                    }
-                }
-
-                // check body if required
-                if let Some(body) = &t.response_body{
-                    let res_body = result.bytes().await;
-                    if res_body.is_err(){
-                        println!("{} ({}) - error getting response body", "fail".red().bold(), i+1);
-                        failed_check = true;
-                    }else{
-                        let res_body = res_body.unwrap();
-                        let res_body_str = res_body.iter().map(|b| *b as char).collect::<String>();
-                        if body_match(body, &res_body_str, i){
-                            failed_check = true;
-                        }
-                    }
-                }
-
-                if failed_check{
-                    failed += 1;
-                }else{
-                    success += 1;
-                    println!("{} ({}) - /{}", "success".green().bold(), i+1, t.request_end_point);
-                }
-            }
-
-            if timeout > 0{
-                std::thread::sleep(Duration::from_millis(timeout as u64));
-            }
-        }
 
         println!("\nResults ({}) -> {}: {} {}: {}",
             test_config,
