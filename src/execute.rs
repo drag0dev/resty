e6403634bc417c1543d08b48ed4bbf45a5ea23d9
Dragoslav Tamindzija <dragodev@protonmail.com>
2022-11-16 11:40:46 +0100

    executing ws tests

--- src/execute.rs
+++ src/execute.rs
@@ -4,9 +4,10 @@ use crate::{
     http_config::MasterStruct,
     http_client::Client,
     helpers::*,
+    ws_config::MessageType,
+    ws_client,
     ws_config,
 };
-
 
 pub async fn http(master_struct: MasterStruct) -> (u32, u32){
     let master_client = Client::new(&master_struct.config);
@@ -93,9 +94,121 @@ pub async fn http(master_struct: MasterStruct) -> (u32, u32){
 }
 
 pub async fn ws(master_struct: ws_config::MasterStruct) -> (u32, u32){
+    let master_client = ws_client::ClientWS::new(&master_struct.config).await;
+    if master_client.is_err(){
+        println!("{}: making a ws client: {}",
+            "error".bold().red(), master_client.err().unwrap());
+        println!("skipping this file...");
+        return (0, 0);
+    }
+    let mut master_client = master_client.unwrap();
     let mut success = 0;
     let mut failed = 0;
+    let timeout = if master_struct.config.timeout.is_some(){
+        master_struct.config.timeout.unwrap()
+    }else{
+        0
+    };
 
+    for (i, t) in master_struct.tests.iter().enumerate(){
+        if (t.response_type.is_none() && t.response_data.is_some()) ||
+           (t.response_type.is_some() && t.response_data.is_none()){
+            println!("{} ({}): response_type and response_data do not align, skipping test", "error".bold().red(), i+1);
+            failed += 1;
+            continue;
+        }
 
+        // execute test
+        let result =
+            if t.send_type == MessageType::Text{
+                let payload = if t.send_data.is_some(){
+                    t.send_data.as_ref().unwrap().clone()
+                }else{
+                    String::from("")
+                };
+                master_client.text(payload).await
+            }else if t.send_type == MessageType::Close{
+                todo!();
+            }else {
+                // these type of messages accept Vec<u8> and not a String
+                let payload = if t.send_data.is_some(){
+                    t.send_data.as_ref().unwrap().as_bytes().to_vec()
+                }else{
+                    Vec::new()
+                };
+                match t.send_type{
+                    MessageType::Binary => master_client.binary(payload).await,
+                    MessageType::Ping => master_client.ping(payload).await,
+                    MessageType::Pong => master_client.pong(payload).await,
+                    _ => unreachable!(),
+                }
+            };
+
+        // check result of the test
+        let mut fail_check = false;
+        if result.is_err(){
+            println!("{}: executing a test{}", "error".red().bold(), i+1);
+            fail_check = true;
+        }else{
+            let result = result.unwrap();
+
+            // if we get a body and expect a body
+            if t.response_type.is_some() && result.is_some(){
+                let result = result.unwrap();
+                if result.is_err(){
+                    println!("\t{} - reading received body", "error".red().bold());
+                    fail_check = true;
+                }else{
+                    let result = result.unwrap();
+
+                    // check type of message
+                    let result_type = message_type(&result);
+                    let test_type = t.response_type.as_ref().unwrap();
+                    if test_type != &result_type{
+                        println!("\t{}: message types not matching {} != {}", "fail".red().bold(), test_type, result_type);
+                        fail_check = true;
+                    }
+
+                    // check bodies
+                    let result_body = result.into_data().iter().map(|b| *b as char).collect::<String>();
+                    let expected_body = t.response_data.as_ref().unwrap();
+                    if body_match(expected_body, &result_body, i){
+                        fail_check = true;
+                    }
+                }
+
+            // if we expect a body but dont recieve any
+            }else if t.response_type.is_some() && result.is_none(){
+                println!("{} ({}) - result missing body",
+                    "error".bold().red(), i+1
+                );
+                fail_check = true;
+            // if we don't expect a body but recieve one
+            }else if t.response_type.is_none() && result.is_some(){
+                println!("{} ({}) - result has a body",
+                    "error".bold().red(), i+1
+                );
+                let result = result.as_ref().unwrap();
+                if result.is_err(){
+                    println!("\t{} - reading received body", "error".red().bold());
+                }else{
+                    println!("\t{}", result.as_ref().unwrap());
+                }
+                fail_check = true;
+            }
+        }
+
+        if fail_check{
+            failed += 1;
+        }else{
+            success += 1;
+            println!("{} ({}) - {}", "success".green().bold(), i+1, t.send_type);
+        }
+
+        // timeout if set
+        if timeout > 0{
+            std::thread::sleep(Duration::from_millis(timeout as u64));
+        }
+    }
     (success, failed)
 }
